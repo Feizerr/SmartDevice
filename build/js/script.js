d2496c38cd73ceb67f442d37c377503fef1c46f3
Feizerr <anastasiapred@gmail.com>
2021-08-13 16:04:14 +0300

    добавляет аккордеон

--- build/js/script.js
+++ build/js/script.js
@@ -1,15 +1,38 @@
-'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
+// 'use strict';
+// var pageHeader = document.querySelector('.page-header');
+// var headerToggle = document.querySelector('.page-header__toggle');
 
-pageHeader.classList.remove('page-header--nojs');
+// pageHeader.classList.remove('page-header--nojs');
 
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
-  } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
-  }
-});
+// headerToggle.addEventListener('click', function () {
+//   if (pageHeader.classList.contains('page-header--closed')) {
+//     pageHeader.classList.remove('page-header--closed');
+//     pageHeader.classList.add('page-header--opened');
+//   } else {
+//     pageHeader.classList.add('page-header--closed');
+//     pageHeader.classList.remove('page-header--opened');
+//   }
+// });
+
+var minHeight = 0;
+var accordionItemHeaders = document.querySelectorAll('.accordion-item-header');
+
+accordionItemHeaders.forEach(accordionItemHeader => {
+    accordionItemHeader.addEventListener('click', event => {
+        var currentlyActiveAccordionItemHeader = document.querySelector('.accordion-item-header.active');
+        if(currentlyActiveAccordionItemHeader && currentlyActiveAccordionItemHeader!==accordionItemHeader) {
+            currentlyActiveAccordionItemHeader.classList.toggle('active');
+            currentlyActiveAccordionItemHeader.nextElementSibling.style.maxHeight = minHeight;
+
+        }
+
+        accordionItemHeader.classList.toggle('active');
+        var accordionItemBody = accordionItemHeader.nextElementSibling;
+        if(accordionItemHeader.classList.contains('active')) {
+            accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + 'px';
+        }
+        else {
+            accordionItemBody.style.maxHeight = minHeight;
+        }
+    });
+})
