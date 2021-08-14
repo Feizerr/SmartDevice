96ca52c54864fc32afeb65fbc62f34a81fcd37a8
Feizerr <anastasiapred@gmail.com>
2021-08-14 21:03:49 +0300

    добавляет блоки с преимуществами и о компании

--- build/js/script.js
+++ build/js/script.js
@@ -1,25 +1,11 @@
-// 'use strict';
-// var pageHeader = document.querySelector('.page-header');
-// var headerToggle = document.querySelector('.page-header__toggle');
-
-// pageHeader.classList.remove('page-header--nojs');
-
-// headerToggle.addEventListener('click', function () {
-//   if (pageHeader.classList.contains('page-header--closed')) {
-//     pageHeader.classList.remove('page-header--closed');
-//     pageHeader.classList.add('page-header--opened');
-//   } else {
-//     pageHeader.classList.add('page-header--closed');
-//     pageHeader.classList.remove('page-header--opened');
-//   }
-// });
+'use strict';
 
 var minHeight = 0;
-var accordionItemHeaders = document.querySelectorAll('.accordion-item-header');
+var accordionItemHeaders = document.querySelectorAll('.accordion__item-header');
 
 accordionItemHeaders.forEach(accordionItemHeader => {
     accordionItemHeader.addEventListener('click', event => {
-        var currentlyActiveAccordionItemHeader = document.querySelector('.accordion-item-header.active');
+        var currentlyActiveAccordionItemHeader = document.querySelector('.accordion__item-header.active');
         if(currentlyActiveAccordionItemHeader && currentlyActiveAccordionItemHeader!==accordionItemHeader) {
             currentlyActiveAccordionItemHeader.classList.toggle('active');
             currentlyActiveAccordionItemHeader.nextElementSibling.style.maxHeight = minHeight;
